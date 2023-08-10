02e6c399976569e2fe04ea06f493e298991a612a
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-10 20:56:08 +0300

    fallback for the favorites page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -5,10 +5,18 @@ import MeetupList from "../components/meetups/MeetupList";
 function FavoritesPage() {
   const favoritesCtx = useContext(FavoritesContext);
 
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
   return (
     <section>
       <h1>My Favorites</h1>
-      <MeetupList meetups={favoritesCtx.favorites} />
+      {content}
     </section>
   );
 }
